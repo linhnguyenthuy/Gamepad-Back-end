281c445e7ed8e6dff0e0791878d0cea4186a67f9
Thuy Linh Nguyen <thuylinhnguyen@air-cua-thuy.lan>
2023-12-05 15:55:57 +0100

    creer la route creator

--- index.js
+++ index.js
@@ -2,27 +2,30 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 
-app.use(cors());
 const app = express();
+
+app.use(cors());
 
 app.use(express.json());
 mongoose.connect("mongodb://localhost/gamepad");
 
-app.get("/games", async (req, res) => {
-  try {
-    const response = await axios.get(
-      `https://api.rawg.io/api/games?apiKey=${process.env.API_KEY}`
-    );
-    res.json(response.data);
-  } catch (error) {
-    res.status(500).json({ message: error.message });
-  }
-});
+// app.get("/games", async (req, res) => {
+//   try {
+//     const response = await axios.get(
+//       `https://api.rawg.io/api/games?apiKey=${process.env.API_KEY}`
+//     );
+//     res.json(response.data);
+//   } catch (error) {
+//     res.status(500).json({ message: error.message });
+//   }
+// });
 
-// const gameRoute = require("./routes/gameRoute");
+const gameRoute = require("./routes/gameRoute");
+const creatorRoute = require("./routes/creatorRoute");
 
-// app.use(gameRoute);
+app.use(gameRoute);
+app.use(creatorRoute);
 
 app.listen(process.env.PORT, () => {
-  console.log("Server started");
+  console.log(`server has start from ${process.env.PORT}`);
 });
--- routes/creatorRoute.js
+++ routes/creatorRoute.js
@@ -5,11 +5,11 @@ const axios = require("axios");
 
 router.use(express.json());
 
-router.get("/games", async (req, res) => {
+router.get("/creators", async (req, res) => {
   try {
     const page = req.query.page || 1;
     const response = await axios.get(
-      `https://api.rawg.io/api/games?key=${process.env.API_KEY}&page=${page}`
+      `https://api.rawg.io/api/creators?key=${process.env.API_KEY}&page=${page}`
     );
     res.json(response.data);
   } catch (error) {
